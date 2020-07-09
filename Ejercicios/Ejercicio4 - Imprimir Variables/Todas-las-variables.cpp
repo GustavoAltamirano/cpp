ffc9d107661b0a6073c162d25864434c988f6f10
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-07-08 23:26:12 -0600

    Añadiendo ejercicio 4

---
+++ Ejercicios/Ejercicio4 - Imprimir Variables/Todas-las-variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorBoolean;
+    valorBoolean = true;
+    int valorEntero = 15;
+    double valorDouble = 20.99;
+    string valorString = "Hola como estan";
+    char valorChar = 'b';
+
+    cout << "Valor Boolean: " << valorBoolean << endl;
+    cout << "Valor Entero: " << valorEntero << endl;
+    cout << "Valor Double: " << valorDouble << endl;
+    cout << "Valor String: " << valorString << endl;
+    cout << "Valor Char: " << valorChar << endl;
+
+    return 0;
+}
