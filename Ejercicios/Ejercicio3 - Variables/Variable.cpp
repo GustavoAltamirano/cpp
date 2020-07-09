ff885dbc1305b299cc993b32af38f2fe90d14fce
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-07-08 23:13:36 -0600

    Añadiendo ejercicio 3

---
+++ Ejercicios/Ejercicio3 - Variables/Variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeroEntero;
+    numeroEntero = 15;
+
+    cout << "Entero: " << numeroEntero << endl;
+
+    numeroEntero = 18;
+    cout << "Entero Modificado: " << numeroEntero;
+
+    return 0;
+}
