34a6a2dcd9b929673b628aaa2652408dbd186770
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-07-24 17:12:26 -0600

    Añadiendo ejercicio26 repaso1

---
+++ Ejercicios/Ejercicio26 - Repaso 1/repaso1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	for (int i = 1; i <= 10; i ++)
+	{
+		cout << "indice: " << i << endl;
+	}
+		
+	cout << endl;
+	cout << endl;
+	
+	int i = 1;
+	
+	while (i <= 10)
+	{
+		cout << "indice: " << i << endl;
+		i ++;	
+	}
+	
+	return 0;
+}
