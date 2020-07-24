21cde42ca4cdb35f7a83ad06a776620dcb331cf0
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-07-24 15:58:22 -0600

    Añadiendo ejercicio22

---
+++ Ejercicios/Ejercicio22 - Ciclos For/CiclosFor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	for (int indice = 1; indice <= 10; indice ++)
+	{
+		cout << "Valor de la variable indice: " << indice << endl;
+	}
+
+	return 0;
+}
