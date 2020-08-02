51a5d8f521e9265e85d732ba534d9a06fa17b1d0
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-08-02 17:32:42 -0600

    Añadiendo ejercicio38

---
+++ Ejercicios/Ejercicio38 - Repaso4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    system ("cls");
+
+    for (int i = 5; i <= 100;i = i + 5)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
