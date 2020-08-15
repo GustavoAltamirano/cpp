39c90eb53ed71d484a1be7e3cc556b4282d829d1
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-08-15 15:33:19 -0600

    Añadiendo ejercicio53

---
+++ Ejercicios/Ejercicio53 - Arrreglos-Unidimensionales/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeros[5]; //declaracion del arreglo
+
+    //asignacion de valores
+    numeros[0] = 10;
+    numeros[1] = 5;
+    numeros[2] = 7;
+    numeros[3] = 50;
+    numeros[4] = 2;
+
+    //salida de los datos
+    cout << numeros[0] << endl;
+    cout << numeros[1] << endl;
+    cout << numeros[2] << endl;
+    cout << numeros[3] << endl;
+    cout << numeros[4] << endl;
+
+    return 0;
+}
