dfef53f9fcf9962c074de90c0744a8fb1d97226f
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-09-18 15:07:49 -0600

    Añadiendo ejercicio2 repaso

---
+++ Ejercicios-L2/Ejercicio02-RepasoMensajes/Mensajes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    string nombre;
+
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+
+    cout << endl;
+    cout << "Hola, mi nombre es " << nombre;
+    cout << endl;
+
+    return 0;
+}
